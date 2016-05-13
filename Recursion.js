0170d97d8d215a414f25c75e59368d10a1993a26
Christine Chou <christinejchou@gmail.com>
2016-05-12 19:01:28 -0700

    Added locate

--- Recursion.js
+++ Recursion.js
@@ -18,4 +18,23 @@ var replaceKeysInObj = function (obj, key, newKey) {
 // replaceKeysInObj(x, 'e', 'f') => {'f': {'x': 'y'}, 't':{'r': {'f':'r'}, 'p': {'y':'r'}}, 'y':'e'}
 
 
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+////// LOCATE
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
 
+// Write a function locate() that takes a value and an array and returns a Boolean 
+// value indicating whether it can be found in a multi-dimentional array.
+
+function locate(array, value){
+
+
+}
+
+// ray1 = [1,2,3,[4,5,[6,7,'a']]];
+// ray2 = [1,[3,['a'],3],6,66,666];
+// ray3 = [[[['a'],4]],6];
+
+// locate(ray1,'a'));  =>  true
+// locate(ray2,'b'); =>  false
