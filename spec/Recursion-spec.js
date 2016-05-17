f7de3087292d54a649f22f992c88db53b88b00a5
Tee Blagdon <tjlay23@gmail.com>
2016-05-16 20:24:09 -0700

    Add binarySearchArray to recursion and recursion-spec tests

--- spec/Recursion-spec.js
+++ spec/Recursion-spec.js
@@ -12,4 +12,41 @@ describe("RECURSION, RECURSION, RECURSION", function() {
     
   });
 
+  describe("LOCATE", function() {
+    
+    ray1 = [1,2,3,[4,5,[6,7,'a']]];
+    ray2 = [1,[3,['a'],3],6,66,666];
+    ray3 = [[[['a'],4]],6];
+
+    it("should return a Boolean", function() {
+      expect(typeof(locate(ray1,'a')).toEqual('boolean');
+    });
+
+    it("should check whether a value is located in nested array", function() {
+      expect(locate(ray1,'a')).toEqual(true);
+      expect(locate(ray2,'b')).toEqual(false);
+    });
+    
+  });
+
+  describe("BINARY SEARCH ARRAY", function() {
+
+  	var sortedArray = [39, 41, 42, 43, 44, 45, 50, 52, 53, 63, 74, 77, 78, 83, 93];
+
+    it("should return a number", function() {
+      expect(typeof(binarySearchArray([2, 3], 0)).toEqual('number');
+    });
+
+    it("should return correct index of number", function() {
+      expect(binarySearchArray(sortedArray, 44)).toEqual(4);
+      expect(binarySearchArray(sortedArray, 39)).toEqual(0);
+    });
+
+    it("should return -1 when number not found", function() {
+      expect(binarySearchArray(sortedArray, 10)).toEqual(-1);
+      expect(binarySearchArray([]], 10)).toEqual(-1);
+    });
+
+  });
+
 });
